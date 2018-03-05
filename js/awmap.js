099e546dc32375d24a4c76b56a72bfdd9b9a6451
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2018-03-05 22:28:02 +0000

    Split navbox code into own file

--- js/awmap.js
+++ js/awmap.js
@@ -23,45 +23,6 @@ function main()
     worldMap.setView(targetLatLng, urlState.zoom);
 }
 
-/** Sets up the top navigational box */
-function setupNavBox()
-{
-    var navBar   = document.querySelector("#navBar");
-    var navInput = navBar.querySelector("input");
-
-    navBar.onsubmit = function(e)
-    {
-        // Needed up here in case this function errors...
-        e.preventDefault();
-
-        // Only zoom in if we're zoomed pretty far out
-        var targetZoom = Math.max( 8, worldMap.getZoom() );
-        var coords     = parseCoords(navInput.value);
-        var latlng     = coords2LatLng(coords);
-
-        navInput.value = coords2PrettyCoords(coords);
-
-        worldMap.setView(latlng, targetZoom);
-        return false;
-    };
-
-    navInput.oninput = function(e)
-    {
-        if (navInput.validity.patternMismatch)
-            navInput.setCustomValidity(
-                'Coordinates must be in one of these formats: ' +
-                '"1000N -1000E", "200N", "-1200s666E", etc.'
-            );
-        else
-            navInput.setCustomValidity('');
-    };
-
-    worldMap.on('moveend', function(e)
-    {
-        navInput.placeholder = latLng2PrettyCoords( worldMap.getCenter() );
-    });
-}
-
 /** Sets up the Leaflet.js map */
 function setupMap()
 {
